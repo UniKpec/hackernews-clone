40f9a6de252466bfeaddac11374bb0e1ee4a0850
Mehmet Akif Akca <meakca10@gmail.com>
2025-07-14 20:05:23 +0300

    The data was modeled,the JSON data was saved into the database and using the ORM, the actual data was rendered in the template.

--- hackernewsclone/hackernews/models.py
+++ hackernewsclone/hackernews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Story(models.Model):
+    title = models.CharField(max_length=150)
+    url = models.URLField(null=True, blank=True)
+    by = models.CharField(max_length=100)
+    descendants = models.IntegerField(null=True , blank=True)
+    score = models.IntegerField(null=True, blank=True)
+    domain = models.CharField(max_length=100, null=True,blank=True)
+    hn_id = models.IntegerField(unique=True) # Hacker News ID
 # Create your models here.
--- hackernewsclone/hackernews/views.py
+++ hackernewsclone/hackernews/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render
+from .models import Story
+from urllib.parse import urlparse
 import requests
 
-
-def top_stroies_show(request):
+def fetch_and_store_stories():
     top_stroies_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     response =requests.get(top_stroies_url)
-    topStroies = response.json()
-    print(response.status_code)
-    top_30_ids = topStroies[:30]
-    stories = []
-    for story_id in top_30_ids:
+    topStroies = response.json()[:30]
+    print("Bıg ",response.status_code)
+    for story_id in topStroies:
         story_url = f"https://hacker-news.firebaseio.com/v0/item/{story_id}/.json"
-        resp = requests.get(story_url)
-        data = resp.json()
-        print(resp.status_code)
-        stories.append({
-            "title":data.get("title"),
-            "url" : data.get("url"),
-            "by": data.get("by"),
-            "descendants" : data.get("descendants")
-    })
-    return render(request,"hackernews/homepage.html",{"stories":stories})
+        story_data = requests.get(story_url).json()
+
+        Story.objects.update_or_create(
+            hn_id = story_id,
+            defaults={
+                "title":story_data.get("title"),
+                "url": story_data.get("url"),
+                "by": story_data.get("by"),
+                "score":story_data.get("score"),
+                'domain': urlparse(story_data.get('url', '')).netloc,  
+                "descendants": story_data.get("descendants", 0),} 
+                
+        ) # eğer descendants verisi jsonda yoksa 0 dondur.
+           
+def top_stroies_show(request):
+        fetch_and_store_stories()  # her istekle API'den veri çeker
+        stories = Story.objects.all()
+        return render(request, 'hackernews/homepage.html', {'stories': stories})
+
 
 def index(request):
     return render(request,"base.html")
